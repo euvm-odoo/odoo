euvm-odoo/odoo

commit ceb8b785fba1055deeff8d9075ccc707f2317278

[FIX] payment_adyen: not send False information
If customer did not have address,email and telephone fields,
"False" would be send to Adyen resulting in these field
containing False values, which in some cases was falsely
triggering anti-fraud system.

task-3159378

closes odoo/odoo#139600

Signed-off-by: anko-odoo <anko@odoo.com>

addons/payment_adyen/utils.py
@@ -1,7 +1,7 @@
 import re
 
 from odoo import _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 from odoo.addons.payment import utils as payment_utils
 
@@ -53,15 +53,20 @@ def format_partner_address(partner):
     """
     STREET_FORMAT = '%(street_number)s/%(street_number2)s %(street_name)s'
     street_data = split_street_with_params(partner.street, STREET_FORMAT)
-    return {
+    address = {
         'city': partner.city,
         'country': partner.country_id.code or 'ZZ',  # 'ZZ' if the country is not known.
-        'stateOrProvince': partner.state_id.code,
+        'stateOrProvince': partner.state_id.code or '', # The state is not always required.
         'postalCode': partner.zip,
         # Fill in the address fields if the format is supported, or fallback to the raw address.
         'street': street_data.get('street_name', partner.street),
         'houseNumberOrName': street_data.get('street_number'),
     }
+    for key, value in address.items():
+        if key == 'stateOrProvince' and partner.country_id.code not in ['CA', 'US', 'GB']:
+            continue
+        if not value:
+            raise ValidationError(_("Please complete your address details."))
 
 
 # The method is copy-pasted from `base_address_extended` with small modifications.

addons/payment_adyen/controllers/main.py
@@ -124,9 +124,9 @@ class AdyenController(http.Controller):
             'recurringProcessingModel': 'CardOnFile',  # Most susceptible to trigger a 3DS check
             'shopperIP': payment_utils.get_customer_ip_address(),
             'shopperInteraction': 'Ecommerce',
-            'shopperEmail': tx_sudo.partner_email,
+            'shopperEmail': tx_sudo.partner_email or "",
             'shopperName': adyen_utils.format_partner_name(tx_sudo.partner_name),
-            'telephoneNumber': tx_sudo.partner_phone,
+            'telephoneNumber': tx_sudo.partner_phone or "",
             'storePaymentMethod': tx_sudo.tokenize,  # True by default on Adyen side
             'additionalData': {
                 'allow3DS2': True

addons/payment_adyen/i18n/payment_adyen.pot
@@ -100,6 +100,12 @@ msgid "Payment Transaction"
 msgstr ""
 
 #. module: payment_adyen
+#: code:addons/payment_adyen/utils.py:0
+#, python-format
+msgid "Please complete your address details."
+msgstr ""
+
+#. module: payment_adyen
 #. openerp-web
 #: code:addons/payment_adyen/static/src/js/payment_form.js:0
 #, python-format

addons/payment_adyen/static/src/js/payment_form.js
@@ -56,7 +56,7 @@ odoo.define('payment_adyen.payment_form', require => {
         _dropinOnError: function (error) {
             this._displayError(
                 _t("Incorrect Payment Details"),
-                _t("Please verify your payment details.")
+                error.message.data.message
             );
         },
 

commit 894c6f7bb02b8609baabc0503bf418f47ebdbd7a

[FIX] payment_adyen: ignore missing street numbers
Commit ceb8b78 introduced an error if street address was
in unexpected format and also function was not returning
the address at all.

closes odoo/odoo#147960

Signed-off-by: anko-odoo <anko@odoo.com>

addons/payment_adyen/utils.py
@@ -60,13 +60,16 @@ def format_partner_address(partner):
         'postalCode': partner.zip,
         # Fill in the address fields if the format is supported, or fallback to the raw address.
         'street': street_data.get('street_name', partner.street),
-        'houseNumberOrName': street_data.get('street_number'),
+        'houseNumberOrName': street_data.get('street_number') or '',
     }
     for key, value in address.items():
         if key == 'stateOrProvince' and partner.country_id.code not in ['CA', 'US', 'GB']:
             continue
-        if not value:
+        elif key == 'houseNumberOrName':
+            continue  # `split_street_with_params` sometimes fails to extract the street number.
+        elif not value:
             raise ValidationError(_("Please complete your address details."))
+    return address
 
 
 # The method is copy-pasted from `base_address_extended` with small modifications.